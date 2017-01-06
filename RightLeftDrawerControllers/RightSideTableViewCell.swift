70c077d1ef11c564a1e1bb06eb71b6b4359e5803
Alex Hu <alexhubnds@icloud.com>
2017-01-06 11:10:03 +0800

    suggestions done

---
+++ RightLeftDrawerControllers/RightSideTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RightSideTableViewCell.swift
+//  BrandeisClassSearchIPHONE
+//
+//  Created by Yuanze Hu on 1/6/17.
+//  Copyright © 2017 Yuanze Hu. All rights reserved.
+//
+
+
+
+import UIKit
+
+class RightSideTableViewCell: UITableViewCell {
+
+
+
+
+}
