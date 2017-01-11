ce99f762e6169dc4260bdf4a7ef71d570ef792c8
Alex Hu <alexhubnds@icloud.com>
2017-01-11 19:32:21 +0800

    add Alamofire and Kanna frameworks with Cocoapods

--- DataSource/CourseDataItem.swift
+++ DataSource/CourseDataItem.swift
@@ -7,10 +7,11 @@
 //
 
 import Foundation
+import Alamofire
+import Kanna
 
 public enum Attribute {
     case NAME, BOOK, BLOCK, TIME, TEACHER, SYLLABUS, DESCRIPTION, TERM, ERROR
-    
     func getHeader() -> String{
         switch  self {
         case .NAME:
@@ -42,22 +43,28 @@ public let attributeList = [Attribute.NAME,Attribute.BOOK,Attribute.BLOCK,Attrib
 class CourseDataItem {
     let Error = "Error"
     var attribute: Attribute
-    
+    var rawInput: String
     var resultList = [String]()
     
-    
+    //no internet connection during init step, nothing is done
+    //resultlList is not empty only if execute() is called
     init(rawDataItem: String) {
         for s in attributeList{
             if rawDataItem.hasPrefix(s.getHeader()){
                 attribute = s
-                //改成task
-                fetchResult()
-                //改成task
+                rawInput = rawDataItem.replacingOccurrences(of: s.getHeader(), with: "").trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+                
                 return
             }
         }
         print("Error in CourseDataItem, rawDataItem: \(rawDataItem)")
+        rawInput = Error
         attribute=Attribute.ERROR
+    }
+    
+    //the method called from outside to start downloading stuffs
+    func execute(){
+        fetchResult()
     }
     
     
@@ -88,7 +95,7 @@ class CourseDataItem {
             doFetchTerm()
             break
         case .ERROR:
-            print("Error can't fetch")
+            print("Error ,can't fetch")
             break
         }
     }
@@ -114,10 +121,11 @@ class CourseDataItem {
     }
     
     private func doFetchSyllabus(){
-        
+        resultList.append(rawInput)
     }
     
     private func doFetchDescription(){
+        
         
     }
     
@@ -126,34 +134,104 @@ class CourseDataItem {
     }
     
 
-    public func doFetch(urlString: String) -> String{
-        var rawHtml = ""
-        let components = NSURLComponents( string: urlString)!
-        
-        let baidu = components.url
-        let request = URLRequest( url: baidu!)
-        print("vvv")
-        let session: URLSession = {
-            let config = URLSessionConfiguration.default
-            return URLSession( configuration: config) }()
-        print("cc")
-        let task = session.dataTask(with: request) { (data, response, error) -> Void in
-            
-            if let jsonData = data {
-                if let jsonString = NSString( data: jsonData, encoding: String.Encoding.utf8.rawValue) {
-                    rawHtml = jsonString as String
-                }
-            } else if let requestError = error {
-                rawHtml = self.Error
-                print(" Error fetching recent photos: \( requestError)")
-            } else {
-                rawHtml = self.Error
-                print(" Unexpected error with the request")
+//    public func doFetch(urlString: String) -> URLSessionDataTask{
+//        var rawHtml = ""
+//        let components = NSURLComponents( string: urlString)!
+//        
+//        let baidu = components.url
+//        let request = URLRequest( url: baidu!)
+//        print("vvv")
+//        let session: URLSession = {
+//            let config = URLSessionConfiguration.default
+//            return URLSession( configuration: config) }()
+//        print("cc")
+//        let task = session.dataTask(with: request) { (data, response, error) -> Void in
+//            
+//            if let htmlData = data {
+//                if let jsonString = NSString( data: htmlData, encoding: String.Encoding.utf8.rawValue) {
+//                    rawHtml = jsonString as String
+//                    print("fetch successful \n*******\n\(rawHtml)\n*******")
+//                    //self.resultList = self.setResult()
+//                }
+//            } else if let requestError = error {
+//                rawHtml = self.Error
+//                print(" Error fetching recent photos: \( requestError)")
+//            } else {
+//                rawHtml = self.Error
+//                print(" Unexpected error with the request")
+//            }
+//            
+//        }
+//        
+//        return task
+//    }
+    
+    private func doFetchWithAlamofire(urlString: String) {
+        Alamofire.request(urlString).responseString(completionHandler: {
+            response in
+            print("is Successful?? \(response.result.isSuccess)")
+            if let html = response.result.value {
+                self.resultList = self.setResult(htmlString: html)
+            }else{
+                print("url not working, url: \(urlString)")
             }
             
+        
+        
+        })
+        
+        
+    }
+    
+    
+    //for now only these three needs to be parsed
+    private func setResult(htmlString:String) -> [String]{
+        switch attribute {
+        case .TEACHER:
+            return parseWithKannaTeacher(htmlString: htmlString)
+        case .DESCRIPTION:
+            return parseWithKannaDescription(htmlString: htmlString)
+        case .BOOK:
+            return parseWithKannaBook(htmlString: htmlString)
+        default:
+            print("setResult is nothing Attri:  \(attribute.getHeader())")
+            return []
         }
-        task.resume()
-        return rawHtml
+    }
+    
+    
+    private func parseWithKannaTeacher(htmlString: String) -> [String] {
+        return []
+    }
+    
+    //the simplest one
+    private func parseWithKannaDescription(htmlString: String) -> [String] {
+        if let doc = Kanna.HTML(html: htmlString, encoding: String.Encoding.utf8) {
+            var desc = [""]
+            var tempString = ""
+            let requirements = doc.css("body span")
+            for s in requirements{
+                tempString.append(s.text!+"  ")
+            }
+            if requirements.count>0{
+                tempString.append("\n")
+            }
+            let body = doc.css("body p")
+            if body.count<1{
+                return []
+            }
+            tempString.append(body[0].text!)
+            desc[0]=tempString
+            return desc
+        }else{
+            print("parseWithKannaDescription Failed  htmlString: \(htmlString)")
+            return []
+        }
+    }
+    
+    private func parseWithKannaBook(htmlString: String) -> [String] {
+        
+        return []
     }
     
     
